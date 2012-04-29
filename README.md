749818dbb2001be86c30abb654412ca1f1cff772
jare <jare@iguanademos.com>
2012-04-30 01:07:51 +0200

    Collection-wide update to server, some cleanup.

--- README.md
+++ README.md
@@ -4,7 +4,7 @@ Example of a Backbone client app with NodeJS server
 This was my first attempt at playing with Backbone, so take it with a grain of salt. It sort of simulates a chatroom, with 2 simultaenous
 views of the users. Every second, each user updates an internal field and sends a message to a random target user.
 
-To install, type 'npm update' to get the packages, then 'node app.js' to run the server, then point your browser to http://localhost:3000/
+To install, type `npm update` to get the packages, then `node app.js` to run the server, then point your browser to http://localhost:3000/index.html
 
 ## Notes
 
@@ -17,11 +17,13 @@ a user is created, it sends a post request. When a user is removed, it sends a d
 the entire collection.
 
 I'm pretty sure the communication with the server should be handled slightly differently, but I don't know how yet. Right now it feels too manual.
+I'm also puzzled by Backbone's lack of a `Collection.save()` function as counterpart to `Collection.fetch()`. It's easy to implement with
+`Backbone.sync("update", collection)` so no big complains, but I don't understand the rationale behind this.
 
 The 'Leave' buttons sometimes do not work because every second, the views are refreshed, and the button destroyed and rebuilt; if you have pressed it but
 not released it yet when this happens, the click will fail. It would be nicer to update just the fields of the view that need to change.
 
-I used underscore on the server just for the convenience _.values() function.
+I used underscore on the server just for the convenience `_.values()` and `_.uniqueId()` functions.
 
 ## Credits & License
 Copyright by Javier Arevalo in 2012.
--- app.js
+++ app.js
@@ -1,48 +1,78 @@
+// Example of a Backbone client app with NodeJS server
+// Copyright by Javier Arevalo in 2012.
+// 
+// - http://www.iguanademos.com/Jare/
+// - @TheJare on twitter
+// 
+// Licensed under the MIT license: http://www.opensource.org/licenses/mit-license.php
+
 var express = require('express')
   , _ = require('underscore')
   , path = require('path')
 
+// Simple express server setup
 var app = express.createServer()
 app.use(express.static(path.join(__dirname, 'static')))
 app.use(express.logger())
 app.use(express.bodyParser())
 
+// Our 'database'
+// A dictionary where the key is an 'id' and the value is an object that contains that id as a field named 'id'.
 var users = {}
 
+// Our REST-oriented api
+
+// Collection-level
+// Fetch the entire collection
 app.get('/api/usersCollection', function(req, res) {
 	console.log("usersCollection api: ", req.method, req.url, req.params, req.body);
-	var v = JSON.stringify(_.values(users));
-	res.send(v);
-	console.log(v);
+	res.send(_.values(users));
+	console.log(users);
+});
+// Update the entire collection
+app.put('/api/usersCollection', function(req, res) {
+	console.log("usersCollection api: ", req.method, req.url, req.params, req.body);
+	_.each(req.body, function(e) { if ('id' in e) users[e.id] = e; });
+	res.send(200);
+	console.log(users);
 });
 
+// Item-level
+// Add item to the collection
 app.post('/api/usersCollection', function(req, res) {
 	console.log("usersCollection api: ", req.method, req.url, req.params, req.body);
-	var uid = _.uniqueId("id_");
+	// Create and assign an id for the new item
+	var uid;
+	do {
+		uid = _.uniqueId("id_");
+	} while (uid in users);
 	users[uid] = req.body;
 	users[uid].id = uid;
 	res.send({ id: uid });
-	console.log(users);
+	//console.log(users);
 });
 
+// Update item
 app.put('/api/usersCollection/:id', function(req, res) {
 	console.log("usersCollection api: ", req.method, req.url, req.params, req.body);
 	users[req.param('id')] = req.body;
-	res.send("ok");
-	console.log(users);
+	res.send(200);
+	//console.log(users);
 });
 
+// Remove item
 app.del('/api/usersCollection/:id', function(req, res) {
 	console.log("usersCollection api: ", req.method, req.url, req.params, req.body);
 	delete users[req.param('id')];
-	res.send("ok");
-	console.log(users);
+	res.send(200);
+	//console.log(users);
 });
 
-// -- Log other types of requests
+// -- Log other types of requests for debugging
 app.all('/api/*', function(req, res) {
 	console.log("api: ", req.method, req.url, req.params, req.body);
-	res.send("ERROR");
+	res.send("Unsupported API", 404);
 });
 
 app.listen(3000)
+console.log("Express listening on http://localhost:3000/");
